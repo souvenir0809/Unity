936e214065a49d8e8b74c67eaf478b96fa3f0e69
林文苏 <1359235023@qq.com>
2024-11-05 17:17:58 +0800

    添加项目文件。

---
+++ Assets/PlayerState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+
+    private string animBoolName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
